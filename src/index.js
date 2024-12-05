72cf3156c09fe216c429e9ff1f6aae5848c76773
Bram <bramstrik@live.com>
2024-12-05 12:33:27 +0100

    Refactor code

--- src/index.js
+++ src/index.js
@@ -1,26 +1,34 @@
 import './index.css'
-import * as api_keys from './api_keys.json'
+import './api_keys.json'
 
-const openWeatherAPI = api_keys.openWeather
+const config = {
+  'key': openWeather,
+  'coords': {
+    'latitude': null,
+    'longitude': null
+  },
+}
+
+function setLocation (pos) {
+  config.coords.latitude = pos.coords.latitude
+  config.coords.longitude = pos.coords.longitude
+}
 
 function getLocation () {
   if (!navigator.geolocation) {
     console.log('Geolocation is not supported by your browser')
     return
   }
-  navigator.geolocation.getCurrentPosition(getWeatherData)
+  navigator.geolocation.getCurrentPosition(setLocation)
 }
 
-function getWeatherData (position) {
-  const lat = position.coords.latitude
-  const lon = position.coords.longitude
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${openWeatherAPI}`
+async function getWeatherData () {
+  if (config.coords.latitude !== null) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${config.coords.latitude}&lon=${config.coords.longitude}&appid=${config.key}`
+    const response = await fetch(url)
+    const weatherData = await response.json()
+  } else {
+    getLocation()
+  }
 
-  fetch(url)
-    .then(response => {
-      return response.json()
-    })
-    .then(weatherData => {
-      // Todo
-    })
 }
