9a54948a855c8f70dc925f84a0696d50682e3aa0
Bram <bramstrik@live.com>
2024-12-05 11:56:26 +0100

    Initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: {
+    script: './src/index.js'
+  },
+  plugins: [
+
+    new HtmlWebpackPlugin({
+      title: 'Todo',
+      template: 'src/index.html'
+    }),
+    new MiniCssExtractPlugin()
+
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  mode: 'development'
+}
