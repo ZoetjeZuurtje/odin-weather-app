8032b0e71c87881dbedc1b56b8ce3bb17dd414dc
Bram <bramstrik@live.com>
2024-12-05 12:52:49 +0100

    Modify getWeatherData func
    Prevents unnecessary API requests by first checking if location data is available, and setting it if it isn't.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import './index.css'
-import './api_keys.json'
+import keys from './api_keys.json'
 
 const config = {
-  'key': openWeather,
-  'coords': {
-    'latitude': null,
-    'longitude': null
-  },
+  key: keys.openWeather,
+  coords: {
+    latitude: null,
+    longitude: null
+  }
 }
 
 function setLocation (pos) {
@@ -14,21 +14,24 @@ function setLocation (pos) {
   config.coords.longitude = pos.coords.longitude
 }
 
-function getLocation () {
+function getLocation (after = () => {}) {
   if (!navigator.geolocation) {
     console.log('Geolocation is not supported by your browser')
     return
   }
-  navigator.geolocation.getCurrentPosition(setLocation)
+  navigator.geolocation.getCurrentPosition((position) => {
+    setLocation(position)
+    after()
+  })
 }
 
 async function getWeatherData () {
-  if (config.coords.latitude !== null) {
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${config.coords.latitude}&lon=${config.coords.longitude}&appid=${config.key}`
-    const response = await fetch(url)
-    const weatherData = await response.json()
-  } else {
-    getLocation()
+  if (config.coords.latitude === null) {
+    getLocation(getWeatherData)
+    return
   }
 
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${config.coords.latitude}&lon=${config.coords.longitude}&appid=${config.key}`
+  const response = await fetch(url)
+  const weatherData = await response.json()
 }
