b5988fdbca41a2cc1ce38fe0823ea40f42fb94b9
Bram <bramstrik@live.com>
2024-12-05 11:58:11 +0100

    Get location data

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ import './index.css'
 import * as api_keys from './api_keys.json'
 
 const openWeatherAPI = api_keys.openWeather
+
+function getLocation () {
+  if (!navigator.geolocation) {
+    console.log('Geolocation is not supported by your browser')
+    return
+  }
+  navigator.geolocation.getCurrentPosition(getWeatherData)
+}
